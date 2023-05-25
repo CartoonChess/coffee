de29873cc1914f3b5baf8bf7b5c796467d8cdbf3
CartoonChess <philip@hkpp.fyi>
2023-05-25 13:01:57 +0900

    Empty-ish coffee classes created

---
+++ src/coffee/WhiteChocolateMocha.java
@@ -0,0 +1,18 @@
+package coffee;
+
+public class WhiteChocolateMocha extends Coffee {
+    @Override
+    protected void ingredient() {
+        // TODO: Auto-generated method stub
+    }
+
+    @Override
+    protected void howToMakeCoffee() {
+        // TODO: Auto-generated method stub
+    }
+
+    @Override
+    protected void prepare() {
+        // TODO: Auto-generated method stub
+    }
+}
