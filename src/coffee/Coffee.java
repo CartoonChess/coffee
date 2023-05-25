bd9b35dc11b55c530063b7c1e5afa2a80d946bc4
CartoonChess <philip@hkpp.fyi>
2023-05-25 12:46:41 +0900

    Fleshed out abstract Coffee class

---
+++ src/coffee/Coffee.java
@@ -0,0 +1,34 @@
+package coffee;
+
+public abstract class Coffee {
+    private String type;
+    private float price;
+    private String storeName;
+
+    // protected abstract String getType();
+    // protected abstract float getPrice();
+    // protected abstract String getStoreName();
+
+    // protected abstract void setType();
+    // protected abstract void setPrice();
+    // protected abstract void setStoreName();
+
+    protected String getType() { return type; }
+    protected float getPrice() { return price; }
+    protected String getStoreName() { return storeName; }
+
+    protected void setType(String type) {
+        this.type = type;
+    }
+
+    protected void setPrice(float price) {
+        this.price = price;
+    }
+    protected void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    protected abstract void ingredient();
+    protected abstract void howToMakeCoffee();
+    protected abstract void prepare();
+}
